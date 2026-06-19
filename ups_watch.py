afdce71a42649be02d5ccd8cf317efbe93fe1c00
scriptedvestige <contact@scriptedvestige.com>
2026-06-19 15:34:27 -0700

    UPS logging

--- ups_watch.py
+++ ups_watch.py
@@ -6,6 +6,9 @@ from sys import argv
 import subprocess
 # import db_ops
 
+CURRENT_DIR = Path.cwd()
+LOG_PATH = CURRENT_DIR / "ups_watch.log"
+
 ups_stats = {'ups_status':'', 'input_voltage':'', 'battery_charge':'', 'battery_runtime':''}
 
 def get_ups_stats():
@@ -14,8 +17,13 @@ def get_ups_stats():
         command = ['upsc', 'cyberpower@10.0.3.8', str(entry.replace("_", "."))]
         ups_stats[entry] = subprocess.run(command, capture_output=True, text=True).stdout.strip()
 
+def write_log(data):
+    """Write data to log."""
+    with open(LOG_PATH, "a") as log:
+        log.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n{ups_stats}')
+
 def print_stats():
-    """Print the current UPS stats."""
+    """Print the current UPS stats to console."""
     for key, value in ups_stats.items():
         print(f'{key.replace(".", " ").title()}: {value}')
 
